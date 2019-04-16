c13aec4f3963439944633c069ed2cdebcd684667
James Pier <jpier2012@gmail.com>
2019-04-16 13:13:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,11 @@
-require 'pry'
-
 class Dog
-  def initialize(name)
-    @@class_class_name = "Doggo"
-    @object_class_name = "Pupper"
-    @name = name
-  end
-
-  def class_class_name=(class_name)
-    @@class_class_name = class_name
-  end
-
-  def object_class_name=(class_name)
-    @object_class_name = class_name
-  end
-
   def name=(name)
     @name = name
-  end
-
-  def class_class_name
-    @@class_class_name
-  end
-
-  def object_class_name
-    @object_class_name
   end
 
   def name
     @name
   end
+
+
 end
-binding.pry
